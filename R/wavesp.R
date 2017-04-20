e5ea4fa3a5a8804c300ca41c464714fb61bfbeb6
millerlp <millerlp@gmail.com>
2017-04-20 11:12:09 -0700

    Moving over the spec.pgram version of this calculation from the notes script

--- R/wavesp.R
+++ R/wavesp.R
@@ -1,6 +1,6 @@
 # Filename: wavesp.R
 # 
-# Author: Luke Miller  Mar 22, 2017
+# Author: Luke Miller  2017-04-20, 2017
 ###############################################################################
 
 
@@ -12,12 +12,19 @@
 ## Spectral wave parameters from pressure transducer data (PT) after 
 ## correction of the pressure attenuation (see function pr_corr)
 #
-## Ported to R from MATLAB by Luke Miller 2016
+## Ported to R from MATLAB by Luke Miller 2017
 ## % written by Urs Neumeier, 2003
 ## % modified from Coast07.m written by T Mason, UPCE, 1999 (psd modified 7/99)
 
+#' Calculate wave parameters using spectral analysis
+#' 
+#' @param PT A vector of surface heights that constitute a time series of 
+#' observations. Units = meters
+#' @param Fs Sampling frequency of the surface heights data. Units = Hz, i.e.
+#' samples per second.
+#' @return Data frame of wave parameters based on spectral methods
 
-wavesp <- function(PT, Zpt, Fs){
+wavesp <- function(PT, Fs){
 	# Inputs
 	# PT = vector of pressure transducer values, converted to seawater height
 	#		(height of sea surface above pressure transducer, units = meters)
@@ -33,56 +40,57 @@ wavesp <- function(PT, Zpt, Fs){
 	# matlab fix() function rounds toward zero, R trunc() function 
 	# should substitute
 #	M = fix(m/Noseg/2)*2;	#% length of segment
-	M <- trunc(m/Noseg/2)*2;	#% length of segment
+#	M <- trunc(m/Noseg/2)*2;	#% length of segment
 	h <- mean(PT);			#% mean water depth
-	PT <- detrend(PT); # MATLAB detrend() function computes the least-squares 
-	# fit of a straight line (or composite line for piecewise linear trends) to 
-	# the data and subtracts the resulting function from the data. The 
-	# detrend() function defined separately in this source file does the
-	# equivalent operation. 
+	PT <- oceanwaves::DetrendHeight(PT); # Call oceanwaves::DetrendHeight() 
+	# function that computes the least-squares fit of a straight line to 
+	# the data and subtracts the resulting function from the data. The resulting
+	# values represent deviations of surface height from the mean surface 
+	# height in the time series.
 	
-	#%***********************************************************%
-	#	% Calculation of spectral density 
-	myspec = spec.pgram(PT, detrend = FALSE)
-	p = myspec$spec
-	Snf = p
-	# Produce the vector of frequencies in Hz from the spectrum object
-	F = myspec$freq * 4
 	
-	# The spectrum function in MATLAB is doing a routine where it steps through
-	# the input data (PT) in windows of length M (900 in Noseg=4) and 
-	# calculating the spectrum using overlapping windows (overlap =450, total 
-	# of 7 windows in a 3600 sample chunk when M = 900). The question is whether
-	# it is worthwhile to implement something similar in R
-	
+	# Use spec.pgram to estimate power spectral density, with smoothers
+	# Convert timeseries of surface heights to a timeseries object
+	xt = ts(PT, frequency = Fs)
+	pgram <- spec.pgram(xt, spans = c(9,9,9), taper=0.1, plot=FALSE)
+	pgramm0 = (Fs*mean(pgram$spec))
+	df = pgram$freq[2]-pgram$freq[1] # bandwidth (Hz) (should be same as values from diff(nyfreqs)
+	integmin=min(which(pgram$freq >= 0)); # this influences Hm0 and other wave parameters
+	min_frequency = 0.05 # from wavesp.m 
+	max_frequency = 0.33 # from wavesp.m 
+	integmax=max(which(pgram$freq <= max_frequency*1.5 ));
+	moment = vector(length = 7)
+	# Calculate moments of the spectrum, from -2nd to 0th to 4th
+	# For a spectrum, the 0th moment represents the variance of the data, and
+	# should be close to the value produced by simply using var(PT)
+	for (i in seq(-2,4,by=1)) { # calculation of moments of spectrum
+		# Note that the pgram$spec values are multiplied by 2 to normalize them
+		# in the same fashion as a raw power spectral density estimator
+		moment[i+3]=sum(pgram$freq[integmin:integmax]^i*
+						(2*pgram$spec[integmin:integmax]))*df;
+	}	
+	m0 = moment[3]; # Estimate variance of timeseries (moment 0)
+	Hm0 = 4 * sqrt(m0) # Estimate sig. wave height based on spectral moment 0, units meters
+	T_0_1 = moment[3]/moment[1+3] # average period m0/m1, units seconds
+	T_0_2 = (moment[0+3]/moment[2+3])^0.5 # average period (m0/m2)^0.5, units seconds
+	T_pc = moment[-2+3]*moment[1+3]/(moment[0+3]^2) # calculated peak period, units seconds
+	EPS2 = (moment[0+3]*moment[2+3]/moment[1+3]^2-1)^0.5;  # spectral width parameter
+	EPS4 = (1 - moment[2+3]^2/(moment[0+3]*moment[4+3]))^0.5;
+	# Peak period, calculated from Frequency at maximum of spectrum 
+	Tp = 1/pgram$freq[which.max(pgram$spec)] # units seconds
+    
+	results = data.frame(h = h, Hm0 = Hm0, Tp = Tp, m0 = m0, T_0_1 = T_0_1,
+			T_0_2 = T_0_2, T_pc = T_pc, EPS2 = EPS2, EPS4 = EPS4)
+
+	# Original wavesp.m call to spectrum() function in MATLAB, for reference
 #	[P,F] <- spectrum(PT,M,M/2,[],Fs);  # % gives spectrum from 0 to Fnyq
 	# The above in MATLAB feeds an empty vector [] to the window argument,
 	# which results in the spectrum() function using hanning(M) to generate
 	# the hanning window. But the hanning() function in MATLAB does not 
 	# included the zero-weighted samples at the start and end of the output
 	# window, while the hann() function does. 
-	
-#	p <- P(2:end,1)*2/Fs;	#% normalization so that psd=cov(7/99), and remove first						
-#	F(1) <- [];		#% element of P and F, which contains no useful information
-	
-#	% frequency range over which the spectrum is integrated for calculation of 
-# the moments
-	integmin=which.min(F[F >= 0])	 #% this influences Hm0 and other wave parameters
-	integmax=which.max(F[F <= (max_frequency*1.5 )])
-	
-	# TODO: I stopped here, since the matlab and R routines are deviating 
-# quite a bit at this point, and numbers aren't coming up similar at all. Take
-# a look back at the spectrum stuff above, since that's where they really 
-# deviate.
-	df = F[1];	#	% bandwidth (Hz)
-	moment = numeric(length = 7)
-	for (i in -2:4){	#		% calculation of moments of spectrum
-		moment[i+3]=sum( (F[integmin:integmax]^i) * Snf[integmin:integmax])*df
-	}
-	#%	fprintf('m%g  =  %g\n',i,moment(i+3));
-	
-	m0 = moment[0+3]
-	
+		
+	results # Return data frame
 }
 
 
