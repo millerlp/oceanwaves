2b5831bbd4ca177a57f0e02021eb5120b41d2640
millerlp <millerlp@gmail.com>
2017-04-21 16:15:48 -0700

    Reworking this to accept output from spectral analysis tools

--- R/plotWaveSpectrum.R
+++ R/plotWaveSpectrum.R
@@ -5,34 +5,29 @@
 
 #' Plot a basic smoothed spectrum 
 #' 
-#' @param PT A vector of surface heights
+#' @param freqspec A data frame containing a column of frequencies and a
+#' column of spectral power values
 #' @param Fs Frequency of sampled surface heights, units of Hz
 
-plotWaveSpectrum <- function(PT, Fs){
-	# Use spec.pgram to estimate power spectral density, with smoothers
-	# Convert timeseries of surface heights to a timeseries object
-	xt = ts(PT, frequency = Fs)
-	pgram <- spec.pgram(xt, spans = c(9,9,9), taper=0.1, plot=FALSE)
-	
-	pgram$spec = 2*pgram$spec # Multiply by 2 to match power spectral density
-	myspec = data.frame(freq = pgram$freq,spec = pgram$spec)
+plotWaveSpectrum <- function(freqspec, Fs){
 	# Extract the frequency associated with the peak spectral density
-	maxfreq = pgram$freq[which.max(pgram$spec)]
+	maxfreq <- freqspec$freq[which.max(freqspec$spec)]
 	# Convert to a period
-	myperiod = 1/maxfreq
+	myperiod <- 1/maxfreq
 	# Also grab max power
-	maxpower = max(pgram$spec)
-	minPeriod = 2 # units of seconds
-	maxPeriod = 20 # units of seconds
+	maxpower <- max(freqspec$spec)
+	minPeriod <- 2 # units of seconds
+	maxPeriod <- 20 # units of seconds
 # Data are at 4Hz interval, so highest possible frequency is half that,
 # meaning 2 Hz interval, 0.5 of 1 second.
-	g.period = rev(c(2,4,6,8,10,12,14,16,18))
-	g.labels = rev(c('2s','4','6','8','10','12','14','16','18')) 
+	g.period <- rev(c(2,4,6,8,10,12,14,16,18))
+	g.labels <- rev(c('2s','4','6','8','10','12','14','16','18')) 
 	
 # Convert the period values into frequencies. 
-	g.freqs = 1/g.period 
+	g.freqs <- 1/g.period 
 	
-	ggplot(data = subset(myspec, freq > 1/(maxPeriod) & freq < 1/(minPeriod) )) + 
+	myplot <- ggplot(data = subset(freqspec, freq > 1/(maxPeriod) & 
+									freq < 1/(minPeriod) )) + 
 			geom_path(aes(x = freq, y = spec)) + 
 			scale_x_log10("Period", breaks = g.freqs, labels = g.labels) +
 			#		scale_y_continuous(limits = ylims) +
@@ -41,4 +36,5 @@ plotWaveSpectrum <- function(PT, Fs){
 			annotate("text", x = maxfreq, y = maxpower, 
 					label = paste('Tp = ',round(myperiod,1),'s'), hjust = 0,
 					vjust = 0)
+	print(myplot)
 }
